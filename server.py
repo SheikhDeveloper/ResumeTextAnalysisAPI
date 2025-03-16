5482b7b70bd73b3350dcc7a4144cf23588a11ceb
Sheikh <tvemperror00@gmail.com>
2025-03-16 22:26:32 +0300

    Fixed bugs in server

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from model_manager import ModelManager
 from flask import Flask, request, make_response
 import requests
 from tf_idf_vectorize import Vectorizer
+import sys
+
+sys.modules['tf_idf_vectorize'] = sys.modules[__name__]
+sys.modules['__main__'].Vectorizer = Vectorizer
 
 app = Flask(__name__)
 
@@ -34,18 +38,19 @@ def predict():
             return response
         json_resp = json_resp['data']
         resume_texts = [record['fields']['Что готов дать компании?'][0] for record in json_resp['records']]
+        record_ids = [record['recordId'] for record in json_resp['records']]
         try:
             results = model_manager.get_prediction(resume_texts)
-            print(results)
         except TypeError:
             response = make_response("Non-text input.")
             response.status_code = 400
             return response
-        result = json.dumps({'ответ модели': ['субъективно' if i == 1 else 'несубъективно' for i in results]})
+        result = json.dumps({'records': [ { 'recordId': record_id, 'fields': {'ответ модели': 'субъективно' if i == 1 else 'несубъективно'} } for record_id, i in zip(record_ids, results)]
+                                          })
         headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
         resp = requests.patch(URL + 'datasheets/' + datasheet_id + '/records', headers=headers, data=result)
 
-        if resp.status_code != 200:
+        if resp.json()['code'] != 200:
             response = make_response(resp.json()['message'])
             response.status_code = 400
             return response
